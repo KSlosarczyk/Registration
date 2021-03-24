8ccffdb4aa2ec253c37601db1f95534839de1dd2
KSlosarczyk <KSlosarczyk@github.com>
2021-03-24 20:44:00 +0100

    Email confirmation template

--- KamilS/src/main/java/com/logandreg/KamilS/email/EmailService.java
+++ KamilS/src/main/java/com/logandreg/KamilS/email/EmailService.java
@@ -4,7 +4,12 @@ import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 @AllArgsConstructor
@@ -14,7 +19,18 @@ public class EmailService implements EmailSender {
     private final JavaMailSender javaMailSender;
 
     @Override
+    @Async
     public void send(String to, String email) {
-
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "UTF-8");
+            mimeMessageHelper.setText(email, true);
+            mimeMessageHelper.setTo(to);
+            mimeMessageHelper.setSubject("Confirm your email");
+            mimeMessageHelper.setFrom("KamilS@register.com");
+        } catch (MessagingException e){
+            LOGGER.error("Failed to send email", e);
+            throw new IllegalStateException("Failed to send email");
+        }
     }
 }
