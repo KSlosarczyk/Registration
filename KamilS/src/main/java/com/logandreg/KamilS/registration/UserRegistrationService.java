1b799d3acce210e3259d1085b2a5050d306fb7f3
KSlosarczyk <KSlosarczyk@github.com>
2021-03-24 18:22:49 +0100

    Added JPA Repository

--- KamilS/src/main/java/com/logandreg/KamilS/registration/UserRegistrationService.java
+++ KamilS/src/main/java/com/logandreg/KamilS/registration/UserRegistrationService.java
@@ -1,5 +1,8 @@
 package com.logandreg.KamilS.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserRegistrationService {
     public String register(UserRegistrationRequest request) {
         return "Its working!";
